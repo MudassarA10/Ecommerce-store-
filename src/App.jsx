7089b087e8cd0b2e99f9deb61b7bac6abca7ee51
Mudassar Ali <mudassarikram834@gmail.com>
2025-05-13 18:43:01 +0100

    commit code
    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+ import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { CartProvider } from "./context/CartContext";
 import { WishlistProvider } from "./context/WishlistContext";
--- src/pages/admin/AdminAddProduct.jsx
+++ src/pages/admin/AdminAddProduct.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
